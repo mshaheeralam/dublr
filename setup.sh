08b0684ff49297e19590e180b390ab53869ec4e3
Afzal Mengal <afzalmengal54@gmail.com>
2023-09-27 02:59:25 +0500

    Removed redundancy

--- setup.sh
+++ setup.sh
@@ -4,13 +4,12 @@ pip install audiotsm
 pip install audio-separator
 sudo apt-get install libsndfile1 -y
 pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
-pip install tiktoken
+pip install tiktoken==0.3.3
 pip install moviepy
 pip install pydub
 sudo apt install gcc -y
 pip install pytorch torchvision torchaudio pytorch-cuda==11.7 -c pytorch -c nvidia
-git clone https://github.com/neonbjb/tortoise-tts.git
-cd tortoise-tts
+
 pip3 install tqdm
 pip3 install rotary_embedding_torch
 pip3 install transformers==4.31.0
@@ -20,7 +19,7 @@ pip3 install progressbar
 pip3 install einops==0.4.1
 pip3 install unidecode
 pip3 install scipy
-pip3 install librosa==0.9.1
+pip3 install librosa==0.9
 pip3 install ffmpeg
 pip3 install numpy
 pip3 install numba
@@ -35,16 +34,13 @@ pip3 install deepspeed
 pip3 install py-cpuinfo
 pip3 install hjson
 pip3 install psutil
-python3 setup.py install
-pip install librosa
-cd ..
-mv tortoise-tts/tortoise tortoise
-rm -rf tortoise-tts 
+pip install soundfile==0.11
+
+pip install TTS
 mkdir ClonedAudio
 mkdir ModifiedAudio
 mkdir AudioChunks
 
-env OPENAI_API_KEY=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
 pip install openai
 
 git clone https://github.com/PrabalS12/DeepFake-LipSync-AI-using-Wav2Lip.git
@@ -55,16 +51,26 @@ pip install https://raw.githubusercontent.com/AwaleSajil/ghc/master/ghc-1.0-py3-
 cd Wav2Lip && pip install -r requirements.txt
 pip install -q youtube-dl
 pip install ffmpeg-python
-pip install librosa==0.9.1
 pip install opencv-python
 apt-get update && apt-get install libgl1 -y
-cd ..
-conda uninstall ffmpeg
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar xvf ffmpeg-git-amd64-static.tar.xz
-cd ffmpeg-git-20230830-amd64-static
-mv ffmpeg 
 
 pip install pymongo
 pip install boto3
 pip install python-dotenv
+
+pip install -qq https://github.com/pyannote/pyannote-audio/archive/refs/heads/develop.zip
+
+sudo apt-get install -y espeak swig libespeak-dev
+wget https://raw.githubusercontent.com/readbeyond/aeneas/master/install_dependencies.sh
+bash install_dependencies.sh
+pip install aeneas
+
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers && sudo make install
+cd ..
+git clone https://git.ffmpeg.org/ffmpeg.git
+sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev libass-dev
+cd ffmpeg
+./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared --enable-libass
+make -j 8
+sudo make install
