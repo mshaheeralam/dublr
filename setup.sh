410e4a1144709c7f54c912ffa1e1c64775e74d72
mshaheeralam <mshaheeralamkz@gmail.com>
2024-01-05 14:24:23 +0500

    Removed unused libraries

--- setup.sh
+++ setup.sh
@@ -19,8 +19,3 @@ pip install openai
 pip install pymongo
 pip install boto3
 pip install python-dotenv
-
-sudo apt-get install -y espeak swig libespeak-dev
-wget https://raw.githubusercontent.com/readbeyond/aeneas/master/install_dependencies.sh
-bash install_dependencies.sh
-pip install aeneas
